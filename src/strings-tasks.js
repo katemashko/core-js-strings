b099aa35090a6c251dbdbee628cb3bdb1fba649e
Katsiaryna Mashko <katemashko@gmail.com>
2023-12-15 20:19:34 +0300

    feat: solve tasks

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -461,8 +461,20 @@ function reverseWords(str) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  let newInvertString = '';
+  let i = 0;
+  while (i < str.length) {
+    let invertChar = str.charAt(i);
+    if (invertChar === invertChar.toUpperCase()) {
+      invertChar = invertChar.toLowerCase();
+    } else {
+      invertChar = invertChar.toUpperCase();
+    }
+    i += 1;
+    newInvertString += invertChar;
+  }
+  return newInvertString;
 }
 
 /**
@@ -478,8 +490,8 @@ function invertCase(/* str */) {
  *   getStringFromTemplate('John','Doe') => 'Hello, John Doe!'
  *   getStringFromTemplate('Chuck','Norris') => 'Hello, Chuck Norris!'
  */
-function getStringFromTemplate(/* firstName, lastName */) {
-  throw new Error('Not implemented');
+function getStringFromTemplate(firstName, lastName) {
+  return `Hello, ${firstName} ${lastName}!`;
 }
 
 /**
@@ -492,8 +504,9 @@ function getStringFromTemplate(/* firstName, lastName */) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  const splitString = value.replace(/[^a-zа-яё0-9\s]/gi, '').split(' ');
+  return `${splitString[1]} ${splitString[2]}`;
 }
 
 /**
@@ -507,8 +520,8 @@ function extractNameFromTemplate(/* value */) {
  *   unbracketTag('<span>') => 'span'
  *   unbracketTag('<a>') => 'a'
  */
-function unbracketTag(/* str */) {
-  throw new Error('Not implemented');
+function unbracketTag(str) {
+  return str.split('').slice(1, -1).join('');
 }
 
 /**
